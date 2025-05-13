924260aa8282ef7c8007a84db21cf10f427cf48d
mouadsifaw <mouaduzumaki2001@gmail.com>
2025-05-12 20:23:08 -0400

    extraction from reddit

--- dags/reddit_dag.py
+++ dags/reddit_dag.py
@@ -1,44 +1,39 @@
-from airflow import DAG
-import datetime
-import sys
 import os
+import sys
+from datetime import datetime
 
+from airflow import DAG
 from airflow.operators.python import PythonOperator
 
-sys.path.insert(0,os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from pipelines.aws_s3_pipeline import upload_s3_pipeline
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
 from pipelines.reddit_pipeline import reddit_pipeline
+
 default_args = {
-    "owner" : "Mouad Sifaw",
-    "start_date": datetime(2025,5,9),
+    'owner': 'Mouad Sifaw',
+    'start_date': datetime(2025, 5, 11)
 }
+
 file_postfix = datetime.now().strftime("%Y%m%d")
 
 dag = DAG(
-    dag_id = "etl_reddit_pipeline",
+    dag_id='etl_reddit_pipeline',
     default_args=default_args,
-    schedule_interval = "@daily",
+    schedule_interval='@daily',
     catchup=False,
-    tags = ['reddit','etl','pipeline']
-
+    tags=['reddit', 'etl', 'pipeline']
 )
-# Extract from reddit
+
+# extraction from reddit
 extract = PythonOperator(
-    task_id = 'reddit_extraction',
-    python_callable = reddit_pipeline,
-    ops_kwargs = {
-        'file_name':f'reddit_{file_postfix}',
-        'subreddit':'dataengineering',
-        'time_filter':'day',
+    task_id='reddit_extraction',
+    python_callable=reddit_pipeline,
+    op_kwargs={
+        'file_name': f'reddit_{file_postfix}',
+        'subreddit': 'dataengineering',
+        'time_filter': 'day',
         'limit': 100
     },
-    dag =dag
+    dag=dag
 )
-# Upload to s3 
-upload_s3 = PythonOperator(
-    task_id = 's3_upload',
-    python_callable = upload_s3_pipeline,
-    dag = dag
-)
-
-extract >> upload_s3
